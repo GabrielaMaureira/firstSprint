bf1a935c5e4f651336f31aa6655ce2178895f65f
GabrielaMaureira <gama@Gabriela-Maureira.local>
2022-12-15 12:23:42 +0100

    Correccion ultima

--- tema4-phpObjects/nivel1/main.php
+++ tema4-phpObjects/nivel1/main.php
@@ -7,8 +7,8 @@ require("Rectangle.php");
 $triangle1 = new Triangle(8,8);
 $rectangle1 = new Rectangle(3,2);
 
-echo $triangle1->calculateAre(). "\n";
-echo $rectangle1->calculateAre();
+echo $triangle1->calculateArea(). "\n";
+echo $rectangle1->calculateArea();
 
 
 
