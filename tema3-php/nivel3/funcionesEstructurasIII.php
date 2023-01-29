734154f38a96c7026470498b18bfb517491caf97
Gaby <gmaureirapalma@gmail.com>
2023-01-29 01:52:04 +0100

    Expression fixed

--- tema3-php/nivel3/funcionesEstructurasIII.php
+++ tema3-php/nivel3/funcionesEstructurasIII.php
@@ -10,13 +10,14 @@ function sieveOfEratosthenes($limit) {
     $primes = array_fill(0, $limit+1, true); 
     
     for ($i = 2; $i <= sqrt($limit); $i++) { 
-        if ($primes[$i] == true) {
+        if ($primes[$i]) {
             for ($x = $i * $i; $x <= $limit; $x += $i) { // Multiples of 20 change to false which it means it's not a prime nº
                 $primes[$x] = false;
                 var_dump($primes);
             }
         }
     }
+    
 }
 
 ?>
